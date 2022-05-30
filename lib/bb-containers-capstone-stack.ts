d44ad822d171aaad89199d1e9f9f52be2f3afe4e
EC2 Default User <ec2-user@ip-172-31-94-255.ec2.internal>
2022-05-30 20:26:30 +0000

    This is the initial cluster setup with various addons

--- lib/bb-containers-capstone-stack.ts
+++ lib/bb-containers-capstone-stack.ts
@@ -1,16 +1,48 @@
-import { Stack, StackProps } from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import * as eks from "aws-cdk-lib/aws-eks";
+import { Construct } from "constructs";
+import * as blueprints from "@aws-quickstart/eks-blueprints";
+import * as ec2 from "aws-cdk-lib/aws-ec2";
 
-export class BbContainersCapstoneStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+export default class ClusterConstruct extends Construct {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id);
 
-    // The code that defines your stack goes here
+    const account = props?.env?.account!;
+    const region = props?.env?.region!;
+    
+    const mgnClusterProviderProps = {
+       amiType: eks.NodegroupAmiType.BOTTLEROCKET_X86_64,
+      desiredSize: 4,  
+      instanceTypes: [
+        new ec2.InstanceType("t3.large"),
+        new ec2.InstanceType("m5.large"),
+      ],      
+      minSize: 1,
+      maxSize: 10,
+      nodeGroupCapacityType: eks.CapacityType.SPOT,
+      version: eks.KubernetesVersion.V1_21
+    }
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'BbContainersCapstoneQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const clusterProvider = new blueprints.MngClusterProvider(mgnClusterProviderProps);
+
+    const blueprint = blueprints.EksBlueprint.builder()
+      .account(account)
+      .region(region)
+      .clusterProvider(clusterProvider)
+      .addOns(
+        new blueprints.AppMeshAddOn(),
+        new blueprints.AwsLoadBalancerControllerAddOn(),
+        new blueprints.NginxAddOn(),
+        new blueprints.ArgoCDAddOn(),
+        new blueprints.CalicoAddOn(),
+        new blueprints.VpcCniAddOn(),
+        new blueprints.KarpenterAddOn(),        
+        new blueprints.KubeviousAddOn(),
+        new blueprints.ContainerInsightsAddOn(),
+        new blueprints.SecretsStoreAddOn()
+      )
+      .teams()
+      .build(scope, id + "stack");
   }
 }
