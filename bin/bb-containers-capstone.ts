0fa68e5fdf7656ce955852ecfbb4952453d06a34
EC2 Default User <ec2-user@ip-172-31-94-255.ec2.internal>
2022-05-31 00:35:59 +0000

    Testing pipeline

--- bin/bb-containers-capstone.ts
+++ bin/bb-containers-capstone.ts
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import BbContainersCapstoneStack  from '../lib/bb-containers-capstone-stack';
+//import BbContainersCapstoneStack  from '../lib/bb-containers-capstone-stack';
+import BbContainersCapstonePipeline from '../lib/bb-containers-capstone-pipeline';
 
 const app = new cdk.App();
 const account = process.env.CDK_DEFAULT_ACCOUNT;
 const region = process.env.CDK_DEFAULT_REGION;
 const env = { account, region };
 
-new BbContainersCapstoneStack(app, 'BbContainersCapstoneStack', { env });
+//new BbContainersCapstoneStack(app, 'BbContainersCapstoneStack', { env });
+new BbContainersCapstonePipeline(app, 'BbContainersCapstonePipeline', { env });
