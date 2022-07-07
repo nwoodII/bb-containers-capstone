fabc5cf32f7c1df43a5c504faf58fdd179fb20c6
EC2 Default User <ec2-user@ip-172-31-94-255.ec2.internal>
2022-07-07 11:56:34 +0000

    Changing ec2 sizing to attempt to force karpenter

--- lib/bb-containers-capstone-pipeline.ts
+++ lib/bb-containers-capstone-pipeline.ts
@@ -16,7 +16,7 @@ export default class BbContainersCapstonePipeline extends Construct {
       amiType: eks.NodegroupAmiType.BOTTLEROCKET_X86_64,
       desiredSize: 1,
       instanceTypes: [
-        new ec2.InstanceType("t3.large")
+        new ec2.InstanceType("t3.medium")
       ],
       minSize: 1,
       maxSize: 10,
