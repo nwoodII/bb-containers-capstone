de30e375d84e7086b8657be0df1e913149e8e474
EC2 Default User <ec2-user@ip-172-31-94-255.ec2.internal>
2022-07-12 15:05:00 +0000

    Adding EBS Driver without explicit version

--- lib/bb-containers-capstone-pipeline.ts
+++ lib/bb-containers-capstone-pipeline.ts
@@ -51,7 +51,7 @@ export default class BbContainersCapstonePipeline extends Construct {
           },
           amiFamily: "Bottlerocket"
         }),
-        new blueprints.EbsCsiDriverAddOn('v1.8.0-eksbuild.0 '),
+        new blueprints.EbsCsiDriverAddOn(),
         new blueprints.KubeviousAddOn(),
         new blueprints.ContainerInsightsAddOn(),
         new blueprints.SecretsStoreAddOn()
